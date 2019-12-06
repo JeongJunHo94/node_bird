aa3977e8f9372101c6e4428682e6a41b7dc0d72b
JeongJunHo94 <JeongJunHo94@gmail.com>
2019-12-06 21:13:34 +0900

    vuetify

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,25 @@
 <template>
-  <div>
-    <nav>
-      <nuxt-link to="/">Home</nuxt-link>
-      <nuxt-link to="/profile">Profile</nuxt-link>
-      <nuxt-link to="/signup">Signnup</nuxt-link>
-    </nav>
-    <nuxt />
-  </div>
+  <v-app>
+    <div>
+      <v-toolbar dark color="green">
+        <v-toobar-title>
+          <nuxt-link to="/">
+            NodeBird
+          </nuxt-link>
+        </v-toobar-title>
+        <v-spacer> </v-spacer>
+        <v-toobar-items>
+          <v-text-field prepend-icon="mdi-magnify" : style="{display : 'flex', alignItems: 'center'}" />
+          <v-btn text nuxt to="/profile" : style="{display : 'flex', alignItems: 'center'}">
+            <div>프로필</div>
+          </v-btn>
+          <v-btn text nuxt to="/signup" : style="{display : 'flex', alignItems: 'center'}">
+            <div>회원가입</div>
+          </v-btn>
+        </v-toobar-items>
+      </v-toolbar>
+    </div>
+  </v-app>
 </template>
 
 <script>
