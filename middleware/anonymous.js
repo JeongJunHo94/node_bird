ae44bea8a7a72783b1daf5bc70ec012df9c84c64
JeongJunHo94 <jcjfnslek@gmail.com>
2019-12-18 15:52:53 +0900

    backend file add

--- middleware/anonymous.js
+++
@@ -1,9 +0,0 @@
-// anonymous.js
-// 로그인을 안한 사용자인지 검사하는 미들웨어
-// 로그인을 한 사용자면 메인페이지로 리다이렉트
-// 미들웨어에서 디스패치나 엑시오스등등 무엇을 할지는 작성자 마음
-export default function({ store, redirect }) {
-  if (store.state.users.me) {
-    redirect("/");
-  }
-}
